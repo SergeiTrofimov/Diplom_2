fc447dd339ebb8bbc383a0ab9f6d776a2ac4c79d
Sergei Trofimov <divisor.null@yandex.com>
2022-11-03 02:27:27 +0300

    1.Начало

--- src/main/java/Setup.java
+++ src/main/java/Setup.java
@@ -1,3 +1,9 @@
 public class Setup {
     private final static String BASE_URI = "https://stellarburgers.nomoreparties.site";
+    private final static String REGISTER_USER = "/api/auth/register";
+    private final static String USER_LOGIN = "/api/auth/login";
+    private final static String USER_LOGOUT = "/api/auth/logout";
+    private final static String USER_RUD = "/api/auth/user";
+    private final static String TOKEN_REFRESH = "/api/auth/token";
+    private final static String CLIENT_ORDERS = "/api/orders";
 }
