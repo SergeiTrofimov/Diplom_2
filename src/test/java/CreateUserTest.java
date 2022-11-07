5e90b5b47901421090321076501c6e8c4d4bab1d
Sergei Trofimov <divisor.null@yandex.com>
2022-11-07 13:04:13 +0300

    1. Написаны тесты по регистрации и логину пользователя

--- src/test/java/CreateUserTest.java
+++ src/test/java/CreateUserTest.java
@@ -11,19 +11,47 @@ public class CreateUserTest {
     UserBodyGenerator generator = new UserBodyGenerator();
     CreatedUser user = generator.generateRandomUser();
 
+
+    // Create user test
     @Test
-    public void createUserTest() {
+    public void createUserTest() { // создаем обычного пользователя
         Response response = userClient.createUserRequest(user);
         response.then().statusCode(200);
         userClient.clearTestData(user);
     }
 
     @Test
-    public void loginUserTest() {
+    public void cantCreateDuplicateUserTest() { // не можем создать дубликат
+        userClient.createUserRequest(user);
+        Response response = userClient.createUserRequest(user);
+        response.then().statusCode(403);
+        userClient.clearTestData(user);
+    }
+
+    @Test
+    public void cantCreateEmptyUserTest() { // не можем создать пользователя с пустым телом
+        CreatedUser user = new CreatedUser(null, null, null);
+        Response response = userClient.createUserRequest(user);
+        response.then().statusCode(403);
+        //  userClient.clearTestData(user);
+    }
+
+
+    @Test
+    public void loginUserTest() { // логин под существующим пользователем
         userClient.createUserRequest(user);
         Response response = userClient.loginUserRequest(user.getEmail(), user.getPassword());
         response.then().statusCode(200);
         response.then().assertThat().body(notNullValue());
         userClient.clearTestData(user);
     }
+
+    @Test
+    public void loginWrongCredentialUserTest() { // логин под существующим пользователем
+        userClient.createUserRequest(user);
+        Response response = userClient.loginUserRequest(user.getEmail(), user.getName());
+        response.then().statusCode(401);
+        userClient.clearTestData(user);
+    }
+
 }
