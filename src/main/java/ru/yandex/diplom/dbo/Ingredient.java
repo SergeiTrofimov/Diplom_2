33eb771068dcaa561a95170c8800f3ac31a60c22
Sergei Trofimov <divisor.null@yandex.com>
2022-11-07 19:13:49 +0300

    1. Написаны тесты по регистрации и логину пользователя 2. Инфраструктура к тесту заказов

--- src/main/java/ru/yandex/diplom/dbo/Ingredient.java
+++ src/main/java/ru/yandex/diplom/dbo/Ingredient.java
@@ -1,4 +1,24 @@
 package ru.yandex.diplom.dbo;
 
-public enum Ingredient {
+public class Ingredient {
+    // Перменные
+    private String id;
+    private String ingredientType;
+
+
+    // Конструктор
+    public Ingredient(String id, String ingredientType) {
+        this.id = id;
+        this.ingredientType = ingredientType;
+    }
+
+    // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getIngredientType() {
+        return ingredientType;
+    }
 }
---
+++ src/main/java/ru/yandex/diplom/generator/OrderBodyGenerator.java
@@ -0,0 +1,4 @@
+package ru.yandex.diplom.generator;
+
+public class OrderBodyGenerator {
+}
