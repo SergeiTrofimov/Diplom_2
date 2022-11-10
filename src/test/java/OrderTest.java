214cd135a33e5af04bbbc3fefecb45c840580ae0
Sergei Trofimov <divisor.null@yandex.com>
2022-11-10 18:00:09 +0300

    1. Написаны тесты по регистрации и логину пользователя 2. Инфраструктура к тесту заказов 3. Тест по заказу

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.junit.After;
@@ -10,6 +11,10 @@ import ru.yandex.diplom.generator.UserBodyGenerator;
 import ru.yandex.diplom.restclient.OrderClient;
 import ru.yandex.diplom.restclient.UserClient;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class OrderTest {
     String token;
     UserClient userClient = new UserClient();
@@ -17,6 +22,7 @@ public class OrderTest {
     UserBodyGenerator userGenerator = new UserBodyGenerator();
     OrderBodyGenerator orderGenerator = new OrderBodyGenerator();
     CreatedUser user = userGenerator.generateRandomUser();
+    Gson gson = new Gson();
 
     @Before
     @Step("Создаем пользователя для тестов, логинимся им, получаем токен")
@@ -33,9 +39,37 @@ public class OrderTest {
     }
 
     @Test
-    public void test() {
+    public void createOrderWithIngredient() {
         String burgerBody = orderGenerator.generateRandomBurger();
         Response orderResponse = orderClient.createOrderRequest(token, burgerBody);
+        orderResponse.then().statusCode(200);
+    }
+
+    @Test
+    public void createOrderWithoutIngredient() {
+        List<String> ingredientList = new ArrayList<>();
+        HashMap<String, List<String>> burgerMap = new HashMap<>();
+        burgerMap.put("ingredients", ingredientList);
+        String burgerBody = gson.toJson(burgerMap);
+        Response orderResponse = orderClient.createOrderRequest(token, burgerBody);
+        orderResponse.then().statusCode(400);
+    }
+
+    @Test
+    public void createOrderWithWrongIngredient() {
+        List<String> ingredientList = new ArrayList<>();
+        ingredientList.add("66678aaa67633");
+        HashMap<String, List<String>> burgerMap = new HashMap<>();
+        burgerMap.put("ingredients", ingredientList);
+        String burgerBody = gson.toJson(burgerMap);
+        Response orderResponse = orderClient.createOrderRequest(token, burgerBody);
+        orderResponse.then().statusCode(500);
+    }
+
+    @Test
+    public void createOrderWithOutToken() {
+        String burgerBody = orderGenerator.generateRandomBurger();
+        Response orderResponse = orderClient.createOrderRequest(" ", burgerBody);
         orderResponse.then().statusCode(200);
     }
 }
